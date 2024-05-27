fd001c3a5b692a74b20e432a078e1ba1604ea371
Vladis <step.gamedevs@gmail.com>
2024-05-27 15:03:41 +0300

    The create Base Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Vladyslav Kliukin (Game Ability System) Project: "Aura"
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Vladyslav Kliukin (Gameplay Ability System) Project: "Aura"
+
+
+#include "Character/AuraEnemy.h"
+
